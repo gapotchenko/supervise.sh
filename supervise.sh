305573af9ed20ac635cca4be48ce7425ff7cb97e
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-10-11 12:33:44 +0200

    Improved documentation.

--- supervise.sh
+++ supervise.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# supervise.sh - a lightweight process-level supervisor.
+# Ensures that the specified command stays running.
+# https://github.com/gapotchenko/supervise.sh
+
 set -eu
 
 # -----------------------------------------------------------------------------
